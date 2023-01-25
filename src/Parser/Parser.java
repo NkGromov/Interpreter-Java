24bab35995e4a44943d395aa917b7cfeaeb14ad9
nikita gromov <gromoff332@gmail.com>
2023-01-25 14:24:08 +0500

    changed imports

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -1,11 +1,7 @@
 package Parser;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.*;
+import java.util.stream.*;
 
 import AST.*;
 import Token.*;
